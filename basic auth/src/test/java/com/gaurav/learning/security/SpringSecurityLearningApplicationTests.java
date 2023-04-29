7aa84164ae726d1b4b55a0b1392ae89630a418c0
Gaurav SUMAN <gaurav.suman@amadeus.com>
2023-04-29 12:01:14 +0530

    Basic Auth project created

---
+++ basic auth/src/test/java/com/gaurav/learning/security/SpringSecurityLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaurav.learning.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
